564cc2f6b961ac515ae55ac0663a515868b39191
cactuskid13 <cactuskid13@gmail.com>
2020-07-23 15:48:26 +0000

    readding scripts, update w ks test

---
+++ scripts/MImat.py
@@ -0,0 +1,190 @@
+#calculat hug MI mat from aln
+
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+
+import itertools
+import gzip
+import multiprocessing as mp
+import lzma
+
+
+import csb
+import pandas as pd
+from Bio import SeqIO
+import pandas as pd
+from colour import Color
+import pickle
+
+import h5py
+
+from Bio import AlignIO
+import dendropy
+
+
+
+
+recalc_sites = False
+NCORE = 55
+
+startk , startl = ( 0, 0 )
+
+
+print('mod7')
+
+
+
+
+
+
+
+
+tree = dendropy.Tree.get(
+
+
+    path='./UKdata/cog_global_2020-05-08_tree.newick',
+    schema='newick')
+
+print(dir(tree))
+
+
+
+for l in tree.leaf_nodes()[0:10]:
+    print(l)
+
+msa = AlignIO.read('./gisaid/msa_0612.fasta' , format = 'fasta')
+print(msa)
+align_array = np.array([list(rec) for rec in msa], np.character)
+print(align_array)
+#meta_data = pd.read_csv('UKdata/cog_2020-05-08_metadata.csv')
+#print(meta_data)
+
+
+"""
+def set_sites( treenode , aln ):
+    return set[  msa[seq , col] ) for col in range(len(msa)) ]
+"""
+if recalc_sites == True:
+    from collections import Counter
+    #sites = { col: Counter( msa[:,col] ) for col in range(len(msa[1,:])) }
+    sites = {}
+    for col in range(len(msa[1,:])):
+        sites.update({col:Counter(msa[:,col])})
+        if col% 1000  == 0:
+            print(col)
+
+
+
+    #sequences = { ID:Counter( msa[i,:]) for i,ID in enumerate(IDs) }
+    sequences = {}
+    for i in range(len(msa)):
+        sequences.update({i:Counter(msa[i,:])})
+        if i% 1000  == 0:
+            print(i)
+
+    #remove uniformative sites from counter
+    #send the informative ones to MI analysis
+    informativesites = [ s for s in sites if len(set( sites[s].keys()) -set(['-','N']) ) > 1  ]
+
+    with open('./UKdata/site_seq_stats.pkl' , 'wb') as pickleout:
+        pickleout.write(pickle.dumps([sites,informativesites, IDs , sequences]) )
+
+
+else:
+    pass
+
+with open('./UKdata/site_seq_stats.pkl' , 'rb') as pickleout:
+    sites,informativesites, IDs , sequences = pickle.loads(pickleout.read())
+#find colum mutual info using compression distance
+#find interprot and intraprot interactions
+
+lzma_filters = my_filters = [
+    {
+      "id": lzma.FILTER_LZMA2,
+      "preset": 9 | lzma.PRESET_EXTREME,
+      "dict_size":len(msa[:,0]) * 40, # a big enough dictionary, but not more than needed, saves memory
+      "lc": 3,
+      "lp": 0,
+      "pb": 0, # assume ascii
+      "mode": lzma.MODE_NORMAL,
+      "nice_len": 273,
+      "mf": lzma.MF_BT4
+    }
+]
+
+def clen(s):
+    return len( lzma.compress(s, format=lzma.FORMAT_RAW, filters= lzma_filters) )
+    #return len(gzip.compress(s))
+
+def compress_dist(pargs):
+    i,j,s1,s2 = pargs
+    strings = [s1.ravel() , s2.ravel() , np.stack([s1,s2]).ravel() ]
+    ls1 , ls2 , ls1_2 = map(clen, strings)
+    return  (i,j,(ls1_2 - min(ls1,ls2) )/ max( ls1,ls2 ) )
+
+def process(q,retq, iolock):
+    #calculate compression distances
+    with iolock:
+        print('init worker')
+    from time import sleep
+    while True:
+        stuff = q.get()
+        if stuff is None:
+            break
+        retq.put(compress_dist(stuff))
+    print('done')
+def mat_creator(retq,matsize,iolock):
+    with iolock:
+        print('init matcreator')
+    #collect distances and create final mat
+    calculations = (matsize**2 - matsize) / 2
+    distmat = np.zeros((matsize,matsize))
+    count = 0
+
+    with h5py.File('./gisaid/alnMI2.h5', 'a') as hf:
+        try:
+            hf.create_dataset("alnMI",  data=distmat)
+        except:
+            pass
+        while True:
+            r = retq.get()
+            count+=1
+            if r is None:
+                break
+            row,col,cdist = r
+            hf['alnMI'][row,col] = cdist
+            if count% 10000 == 0 :
+                with iolock:
+                    print(count/calculations)
+                    print((row,col))
+                    hf.flush()
+    print('done saver')
+
+
+if __name__ == '__main__':
+    q = mp.Queue(maxsize=NCORE*5000)
+    retq = mp.Queue(maxsize=NCORE*5000)
+    iolock = mp.Lock()
+    pool = mp.Pool(NCORE, initializer=process, initargs=(q,retq, iolock))
+    p = mp.Process(target=mat_creator, args=(retq,len(msa[1,:]), iolock))
+    p.start()
+    for k,i in enumerate(informativesites):
+        for l,j in enumerate(informativesites):
+            if k < l and k > startk and l > startl :
+                s1 = align_array[:,i].ravel()
+                s2 = align_array[:,j].ravel()
+                q.put( (i,j,s1,s2) )
+                if k % 100 == 0 and l %100 == 0:
+                    print((k,l))
+                    print((i,j))
+
+    for _ in range(NCORE):  # tell workers we're done
+        q.put(None)
+
+    #pool.join()
+
+    #retq.put(None)
+    #pool.close()
+    p.join()
---
+++ scripts/coevsankoff.py
@@ -0,0 +1,265 @@
+import numpy as np
+from collections import Counter
+import multiprocessing as mp
+import pandas as pd
+from Bio import SeqIO
+import pandas as pd
+from colour import Color
+import pickle
+import time
+import h5py
+import dendropy
+from Bio import AlignIO , SeqIO
+from scipy import sparse
+import pickle
+import multiprocessing as mp
+import copy
+import sys
+import os
+
+sys.setrecursionlimit( 10 **5 )
+
+runName = 'COEV_cleantree'
+NCORE = 60
+treefile = '/home/cactuskid13/covid/coevDataCOVID/02_07_2020_GISAID_IQTree_NS_mask/gisaid_hcov-2020_07_02.QC.NSoutlier.filter.NSmask.aln.treefile'
+alnfile = '/home/cactuskid13/covid/coevDataCOVID/02_07_2020_GISAID_IQTree_NS_mask/gisaid_hcov-2020_07_02.QC.NSoutlier.filter.NSmask.aln'
+
+
+
+tree = dendropy.Tree.get(
+    path=treefile,
+    schema='newick')
+
+for l in tree.leaf_nodes()[0:10]:
+    print(str(l.taxon))
+
+
+msa = AlignIO.read(alnfile , format = 'fasta')
+if os.path.exists(alnfile +'.h5'):
+    with h5py.File(alnfile +'.h5', 'r') as hf:
+        align_array = hf['MSA2array'][:]
+        #implement w np unique could be faster
+else:
+    print('aln 2 numpy ')
+    align_array = np.array([list(rec) for rec in msa], np.character)
+    print('done')
+    print('dumping to hdf5')
+    with h5py.File(alnfile +'.h5', 'w') as hf:
+        hf.create_dataset("MSA2array",  data=align_array)
+    print('done')
+
+sites = {}
+
+for col in range(align_array.shape[1]):
+    if col% 1000  == 0:
+        print(col)
+    (unique, counts) = np.unique(align_array[:,col].ravel() , return_counts=True)
+    sites.update({col:dict(zip(list(unique), list(counts)))})
+informativesites = [ s for s in sites if len(set( sites[s].keys()) -set([b'-',b'N']) ) > 2  ]
+print(len(informativesites))
+
+print('rev3')
+import pdb; pdb.set_trace()
+
+
+#def clipID(ID):
+#    return ''.join( [ s +'|' for s in str(ID).split('|')[:-1] ])[:-1].replace('_',' ')
+
+def clipID(ID):
+    return ID.replace('|',' ').replace('_',' ').replace('/',' ')
+
+ #'msa :::: hCoV-19/Iceland/447/2020|EPI_ISL_424471|2020-03-20'
+
+# 'tree :::: hCoV-19 Scotland EDB399 2020 EPI ISL 425998 2020-03-30'
+
+IDs = {i:clipID(rec.id) for i,rec in enumerate(msa)}
+
+#IDs = {i:rec.id for i,rec in enumerate(msa)}
+IDindex = dict(zip( IDs.values() , IDs.keys() ) )
+print( [(t,IDindex[t]) for t in list(IDindex.keys())[0:10]] )
+print(dir(tree))
+
+#prep tree for sankoff
+for l in tree.leaf_nodes()[0:10]:
+    print(str(l.taxon))
+print(len(tree.leaf_nodes()))
+print('leaves')
+for i,n in enumerate(tree.nodes()):
+    n.matrow = i
+    n.symbols = None
+    n.scores = None
+    n.event = None
+    n.char = None
+
+matsize = len(tree.nodes())
+print(matsize)
+print('nodes')
+#sites = { col: Counter( msa[:,col] ) for col in range(len(msa[1,:])) }
+#place an event on a column with multiple symbols
+allowed_symbols = { b'A', b'C', b'G' , b'T' }
+
+def process_node_smallpars_1(node):
+    #go from leaves up and generate character sets
+    if node.symbols is None:
+        for child in node.child_nodes():
+            if child.symbols is None:
+                process_node_smallpars_1(child)
+        symbols = set.intersection( * [ child.symbols for child in node.child_nodes( ) ] )
+        if len(symbols) == 0:
+            symbols = set.union( * [ child.symbols for child in node.child_nodes( ) ] )
+
+
+        node.symbols = symbols
+        node.scores = { }
+
+        for c in allowed_symbols:
+            if c not in node.symbols:
+                #add trnasition mat here if needed
+                score = min(  [ child.scores[c] for child in node.child_nodes()])+1
+            else:
+                score = min(  [ child.scores[c] for child in node.child_nodes() ] )
+            node.scores[c] = score
+
+def process_node_smallpars_2(node):
+    #assign the most parsimonious char from children
+    if node.char is None:
+
+        node.char = min(node.scores, key=node.scores.get)
+
+        if node.parent_node:
+            if node.parent_node.char == node.char:
+                node.event = 0
+            else:
+                if node.scores[node.parent_node.char] == node.scores[node.char] :
+                    node.char = node.parent_node.char
+                    node.event = 0
+                else:
+                    node.event = 1
+        else:
+            node.event = 0
+        for child in node.child_nodes():
+            if child.char is None:
+                process_node_smallpars_2(child)
+def calculate_small_parsimony( t, aln_column , row_index , verbose  = False ):
+    missing = 0
+    #assign leaf values
+    for l in t.leaf_nodes():
+        l.event = 0
+        try:
+            char = aln_column[row_index[str(l.taxon).replace("'" , '' )]]
+        except KeyError:
+            missing += 1
+            char = None
+        if char in allowed_symbols:
+            l.symbols = { char }
+        else:
+            #ambiguous leaf
+            l.symbols =  allowed_symbols
+        l.scores = { c:0 if c in l.symbols else 10**10 for c in allowed_symbols }
+        l.char = min(l.scores, key=l.scores.get)
+    process_node_smallpars_1(t.seed_node)
+
+    #down
+    process_node_smallpars_2(t.seed_node)
+    if verbose == True:
+        print('done init')
+        print(missing)
+        print('missing in aln')
+        for n in t.nodes()[0:5]:
+            print(n)
+            print(n.symbols)
+            print(n.scores)
+            print(n.char)
+            print(n.event)
+    eventindex = [ n.matrow for n in t.nodes()   if n.event > 0 ]
+    if verbose == True:
+        print(eventindex)
+    events = np.zeros( (len(t.nodes()),1) )
+    events[eventindex] = 1
+    return events
+
+def process(q,retq, iolock ,tree , IDindex   ):
+    #calculate tree events
+    with iolock:
+        print('init worker')
+    count = 0
+    while True:
+        stuff = q.get()
+        if stuff is None:
+            break
+        index,aln_column = stuff
+
+        retq.put((index,calculate_small_parsimony( copy.deepcopy(tree) , aln_column , IDindex ) ) )
+        if count % 1000 == 0:
+            with iolock:
+                print(index)
+                print('worker')
+        count+= 1
+    print('done')
+
+def mat_creator(retq,matsize,iolock,runName = ''):
+    with iolock:
+        print('init matcreator')
+    #collect distances and create final mat
+    #distmat = np.zeros(  )
+    M1 = sparse.lil_matrix((matsize[0],matsize[1] ))
+    count = 0
+    init = False
+    t0 = time.time()
+    while True:
+        r = retq.get()
+        if r is not None:
+            init = True
+
+        count+=1
+        if r is None and init == True:
+            break
+        col,events = r
+        #import pdb; pdb.set_trace()
+        M1[np.nonzero(events)[0] ,col] = 1
+        if count == 0:
+            with iolock:
+                print(np.nonzero(events))
+
+        if time.time()-t0> 120 :
+            t0 = time.time()
+            with iolock:
+                print('saving')
+                print(col)
+                with open( runName + 'coevmatrev3.pkl' , 'wb') as coevout:
+                    coevout.write(pickle.dumps(M1))
+                print('done')
+
+def main():
+
+
+    #reset tree
+    for i,n in enumerate(tree.nodes()):
+        n.matrow = i
+        n.symbols = None
+        n.scores = None
+        n.event = None
+        n.char = None
+    #our results will be events on tree nodes for each column
+    matsize = (len(tree.nodes()),align_array.shape[1])
+    q = mp.Queue(maxsize=NCORE*1000)
+    retq = mp.Queue(maxsize= NCORE*100 )
+    iolock = mp.Lock()
+    #start workers
+    pool = mp.Pool(NCORE, initializer=process, initargs=(q,retq, iolock ,tree , IDindex ))
+    #start saver
+    p = mp.Process(target=mat_creator, args=(retq,matsize, iolock, alnfile + runName ))
+    p.start()
+    for i,k in enumerate(informativesites):
+        s1 = align_array[:,k]
+        q.put( (k,s1) )
+    for _ in range(NCORE):  # tell workers we're done
+        q.put(None)
+    #retq.put(None)
+    pool.close()
+    pool.join()
+    #retq.put(None)
+    p.join()
+
+if __name__ == '__main__':
+    main()
