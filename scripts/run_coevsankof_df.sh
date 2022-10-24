95968caf26422cd9d80b0ef8c010dc0fec407713
cactuskid <cactuskid13@gmail.com>
2022-10-24 21:49:47 +0200

    adding notebooks

--- scripts/run_coevsankof_df.sh
+++ scripts/run_coevsankof_df.sh
@@ -11,5 +11,6 @@ source /work/FAC/FBM/DBC/cdessim2/default/dmoi/condaenvs/etc/profile.d/conda.sh
 conda activate ML2
 
 
+
 # Run Jupyter
-python coevsankoff_distributed_delayed_remoteopen.py
+python coevsankoff_distributed_nx.py
